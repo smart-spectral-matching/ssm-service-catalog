29f7964de5bd460ee20e1b89422be4ee20ed2935
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-11 10:45:27 -0400

    Fix the web client test class name

--- nuclear-datastreams/web-client/src/test/java/gov/ornl/rse/datastreams/nuclear_datastreams/web_client/WebClientAppTest.java
+++ nuclear-datastreams/web-client/src/test/java/gov/ornl/rse/datastreams/nuclear_datastreams/web_client/WebClientAppTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class WebClientApp 
+public class WebClientAppTest 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class WebClientApp
      *
      * @param testName name of the test case
      */
-    public WebClientApp( String testName )
+    public WebClientAppTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class WebClientApp
      */
     public static Test suite()
     {
-        return new TestSuite( WebClientApp.class );
+        return new TestSuite( WebClientAppTest.class );
     }
 
     /**
