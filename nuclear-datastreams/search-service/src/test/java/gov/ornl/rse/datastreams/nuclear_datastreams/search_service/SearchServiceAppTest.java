ff24382698f75ac7f806ae3c486582b4f8e371dc
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-11 10:46:57 -0400

    Add skeleton of the search-service sub-module

---
+++ nuclear-datastreams/search-service/src/test/java/gov/ornl/rse/datastreams/nuclear_datastreams/search_service/SearchServiceAppTest.java
@@ -0,0 +1,38 @@
+package gov.ornl.rse.datastreams.nuclear_datastreams.search_service;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class SearchServiceAppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public SearchServiceAppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( SearchServiceAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
