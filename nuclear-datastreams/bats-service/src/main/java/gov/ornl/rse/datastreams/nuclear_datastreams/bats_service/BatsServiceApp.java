df7fbbea6b96de5d40790b520b1904bce1a787b3
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-11 10:57:43 -0400

    Adding skeleton of bats-service sub-module

---
+++ nuclear-datastreams/bats-service/src/main/java/gov/ornl/rse/datastreams/nuclear_datastreams/bats_service/BatsServiceApp.java
@@ -0,0 +1,13 @@
+package gov.ornl.rse.datastreams.nuclear_datastreams.bats_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class BatsServiceApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ nuclear-datastreams/bats-service/src/test/java/gov/ornl/rse/datastreams/nuclear_datastreams/bats_service/BatsServiceAppTest.java
@@ -0,0 +1,38 @@
+package gov.ornl.rse.datastreams.nuclear_datastreams.bats_service;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BatsServiceAppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BatsServiceAppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BatsServiceAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
