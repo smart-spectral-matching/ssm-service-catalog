8b01e71e7c1306d36ba3f598163b125a0680420f
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-11 10:37:14 -0400

    Initial addition of skeleton for parent app

---
+++ nuclear-datastreams/src/main/java/gov/ornl/rse/datastreams/nuclear_datastreams/NuclearDatastreamsApp.java
@@ -0,0 +1,13 @@
+package gov.ornl.rse.datastreams.nuclear_datastreams;
+
+/**
+ * Hello world!
+ *
+ */
+public class NuclearDatastreamsApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
