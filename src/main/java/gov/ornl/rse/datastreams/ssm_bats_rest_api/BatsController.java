3e6117d3bdfb1280c7c77f39a799b38815b6e395
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-10-16 19:12:03 -0400

    Fix GET method + tabs -> spaces

--- src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/BatsController.java
+++ src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/BatsController.java
@@ -38,13 +38,13 @@ import gov.ornl.rse.bats.DataSet;
 @RestController
 @RequestMapping("/datasets")
 public class BatsController {
-	private static final Logger logger = LoggerFactory.getLogger(BatsController.class);
+    private static final Logger logger = LoggerFactory.getLogger(BatsController.class);
 
     private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
 
-	private String hostname = "http://rse-nds-dev1.ornl.gov";
+    private String hostname = "http://rse-nds-dev1.ornl.gov";
 
-	private static String bytesToHex(byte[] bytes) {
+    private static String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
             int v = bytes[j] & 0xFF;
@@ -55,14 +55,14 @@ public class BatsController {
     }
 
     public static String generateUUID() throws NoSuchAlgorithmException, UnsupportedEncodingException {
-		MessageDigest salt = MessageDigest.getInstance("SHA-256");
-		salt.update(
-			UUID.randomUUID()
-				.toString()
-				.getBytes("UTF-8"));
-		String digest = bytesToHex(salt.digest());
-		return digest;
-	}
+        MessageDigest salt = MessageDigest.getInstance("SHA-256");
+        salt.update(
+            UUID.randomUUID()
+                .toString()
+                .getBytes("UTF-8"));
+        String digest = bytesToHex(salt.digest());
+        return digest;
+    }
 
     void write(OutputStream out, DatasetGraph g, RDFFormat f, Context ctx) {
         RDFWriter w =
@@ -78,7 +78,7 @@ public class BatsController {
         write(System.out, g, f, ctx);
     }
 
-	private String write2String(DatasetGraph g, RDFFormat f, Context ctx) {
+    private String write2String(DatasetGraph g, RDFFormat f, Context ctx) {
         try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
             write(out, g, f, ctx);
             out.flush();
@@ -86,52 +86,51 @@ public class BatsController {
         } catch (IOException e) { throw new RuntimeException(e); }
     }
 
-	// GET
-	@RequestMapping(value = "/{uuid}", method = RequestMethod.GET)
-	public String getDataSetById(@PathVariable("uuid") String uuid) {
-		String jsonld = new String();
-		try {
-		    DataSet dataset = new DataSet();
-		    dataset.setHost(hostname);
-			Model model = dataset.getModel(uuid);
+    // GET
+    @RequestMapping(value = "/{uuid}", method = RequestMethod.GET)
+    public String getDataSetById(@PathVariable("uuid") String uuid) {
+        String jsonld = new String();
+        try {
+            DataSet dataset = new DataSet();
+            dataset.setName(uuid);
+            dataset.setHost(hostname);
+            Model model = dataset.getModel(uuid);
 
-			DatasetGraph g = DatasetFactory.wrap(model).asDatasetGraph();
-			JsonLDWriteContext ctx = new JsonLDWriteContext();
-			jsonld = write2String(g, RDFFormat.JSONLD_COMPACT_PRETTY, ctx);
-		} catch (Exception e) {
-			logger.error("Unable to get model on the remote Fuseki server.", e);
-		}
-		return jsonld;
-	}
+            DatasetGraph g = DatasetFactory.wrap(model).asDatasetGraph();
+            JsonLDWriteContext ctx = new JsonLDWriteContext();
+            jsonld = write2String(g, RDFFormat.JSONLD_COMPACT_PRETTY, ctx);
+        } catch (Exception e) {
+            logger.error("Unable to get model on the remote Fuseki server.", e);
+        }
+        return jsonld;
+    }
 
-	// POST
-	@RequestMapping(value = "", method = RequestMethod.POST)
-	public String createDataset(@RequestBody String jsonPayload) throws Exception {
-		// JSON -> Tree
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode treeNode = mapper.readTree(jsonPayload);
+    // POST
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String createDataset(@RequestBody String jsonPayload) throws Exception {
+        // JSON -> Tree
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode treeNode = mapper.readTree(jsonPayload);
 
-		// add stuff to tree here
-		String uuid = generateUUID();
+        // add stuff to tree here
+        String uuid = generateUUID();
 
-		// Tree -> JSON -> Jena Model
+        // Tree -> JSON -> Jena Model
         StringReader reader = new StringReader(treeNode.toString());
-		Model model = ModelFactory.createDefaultModel().read(reader, null, "JSON-LD");
+        Model model = ModelFactory.createDefaultModel().read(reader, null, "JSON-LD");
 
-		// Jena Model -> BATS DataSet
-		DataSet dataset = new DataSet();
-		dataset.setName(uuid);
-		dataset.setHost(hostname);
-		dataset.create();
-		System.out.println(dataset.getFullURI());
-		try { 
+        // Jena Model -> BATS DataSet
+        DataSet dataset = new DataSet();
+        dataset.setName(uuid);
+        dataset.setHost(hostname);
+        dataset.create();
+        System.out.println(dataset.getFullURI());
+        try {
             dataset.updateModel(uuid, model);
-			logger.debug("Model uploaded!");
-		} catch (Exception e) {
-			logger.error("Unable to update model on the remote Fuseki server.", e);
-		}
-
-
-		return uuid;
-	}   
+            logger.debug("Model uploaded!");
+        } catch (Exception e) {
+            logger.error("Unable to update model on the remote Fuseki server.", e);
+        }
+        return uuid;
+    }
 }
