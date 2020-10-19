ef26e66a0de19b57c551b63dc13cfa1728c52a45
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-10-19 17:02:02 -0400

    Create separate classes for uuid and jsonld writing

---
+++ src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/UUIDGenerator.java
@@ -0,0 +1,32 @@
+package gov.ornl.rse.datastreams.ssm_bats_rest_api;
+
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
+
+
+public class UUIDGenerator {
+    private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    public static String generateUUID() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest salt = MessageDigest.getInstance("SHA-256");
+        salt.update(
+            UUID.randomUUID()
+                .toString()
+                .getBytes("UTF-8"));
+        String digest = bytesToHex(salt.digest());
+        return digest;
+    }
+}
