2b54b48b6c3f6d1adf7e9ddaf2d8559dfaad513c
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-01-25 16:02:46 -0500

    Partial lint fixes for UUIDGenerator

--- src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/UUIDGenerator.java
+++ src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/UUIDGenerator.java
@@ -1,25 +1,43 @@
 package gov.ornl.rse.datastreams.ssm_bats_rest_api;
 
-
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 
-public class UUIDGenerator {
-    private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
+public final class UUIDGenerator {
+    /**
+     * Constructor set to private since this is a utility class.
+    */
+    private UUIDGenerator() { };
 
-    private static String bytesToHex(byte[] bytes) {
+    /**
+     * Hex array to use for generating UUID.
+    */
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
+    /**
+     * 0xFF - Mask for 8 bit values.
+    */
+    private static final int MASKER = 0xFF;
+
+    private static String bytesToHex(final byte[] bytes) {
+        final int FOUR = 4;
         char[] hexChars = new char[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+            int v = bytes[j] & MASKER;
+            hexChars[j * 2] = HEX_ARRAY[v >>> FOUR];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
         }
         return new String(hexChars);
     }
 
+    /**
+     * Returns a unique UUID.
+     *
+     * @return Random UUID as string
+    */
     public static String generateUUID() throws NoSuchAlgorithmException, UnsupportedEncodingException {
         MessageDigest salt = MessageDigest.getInstance("SHA-256");
         salt.update(
