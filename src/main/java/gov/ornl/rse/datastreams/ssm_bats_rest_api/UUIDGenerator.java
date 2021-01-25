12144e6d9e38d674572c182404d09990ca1298cc
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-01-25 16:46:15 -0500

    Lint fixes fo UUIDGenerator and BatsDataset

--- src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/UUIDGenerator.java
+++ src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/UUIDGenerator.java
@@ -18,27 +18,45 @@ public final class UUIDGenerator {
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
     /**
-     * 0xFF - Mask for 8 bit values.
+     * Shift value for hex characters from hex array.
     */
-    private static final int MASKER = 0xFF;
+    private static final int SHIFT = 4;
 
+    /**
+     * Mask for 8 bit for index value.
+    */
+    private static final int MASK_INDEX = 0xFF;
+
+    /**
+     * Mask for 8 bit for index value.
+    */
+    private static final int MASK_HEX_ARRAY = 0x0F;
+
+    /**
+     * Returns the hex for a given set of bytes.
+     *
+     * @param bytes Bytes to convert to hex
+     * @return      Hex of the given bytes as a string
+    */
     private static String bytesToHex(final byte[] bytes) {
-        final int FOUR = 4;
         char[] hexChars = new char[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & MASKER;
-            hexChars[j * 2] = HEX_ARRAY[v >>> FOUR];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+            int v = bytes[j] & MASK_INDEX;
+            hexChars[j * 2] = HEX_ARRAY[v >>> SHIFT];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & MASK_HEX_ARRAY];
         }
         return new String(hexChars);
     }
 
     /**
-     * Returns a unique UUID.
+     * Returns a unique UUID as a string.
      *
      * @return Random UUID as string
     */
-    public static String generateUUID() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public static String generateUUID()
+        throws
+            NoSuchAlgorithmException,
+            UnsupportedEncodingException {
         MessageDigest salt = MessageDigest.getInstance("SHA-256");
         salt.update(
             UUID.randomUUID()
--- src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/models/BatsDataset.java
+++ src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/models/BatsDataset.java
@@ -1,30 +1,70 @@
 package gov.ornl.rse.datastreams.ssm_bats_rest_api.models;
 
+/**
+ * Representation of a BATS Dataset for Apache Jena Dataset
+ * A Dataset holds a collection of Models.
+*/
+
 public class BatsDataset {
+    /**
+     * UUID for the Dataset.
+    */
     private String uuid;
+
+    /**
+     * URI for the Dataset in Fuseki server.
+     * TODO: Remove this field
+    */
     private String uri;
 
-    public BatsDataset(String uuid, String uri) {
+    /**
+     * Constructor class to create a BatsDataset object.
+     *
+     * @param datasetUUID UUID for the new Dataset
+     * @param datasetURI  URI for the new Dataset in Fuseki database
+    */
+    public BatsDataset(final String datasetUUID, final String datasetURI) {
         super();
-        this.uuid = uuid;
-        this.uri = uri;
+        this.uuid = datasetUUID;
+        this.uri = datasetURI;
     }
 
     // API
 
+    /**
+     * Getter for the Dataset's UUID.
+     *
+     * @return UUID for the Dataset
+    */
     public String getUUID() {
         return uuid;
     }
 
-    public void setUUID(final String uuid) {
-        this.uuid = uuid;
+    /**
+     * Setter for the Dataset's UUID.
+     *
+     * @param datasetUUID New dataset UUID to set BatsDataset
+    */
+
+    public void setUUID(final String datasetUUID) {
+        this.uuid = datasetUUID;
     }
 
+    /**
+     * Getter for the Dataset URI.
+     *
+     * @return URI for the dataset in Fuseki database
+    */
     public String getURI() {
         return uri;
     }
 
-    public void setURI(final String uri) {
-        this.uri = uri;
+    /**
+     * Setter for the Dataset URI.
+     *
+     * @param datasetURI New URI for Dataset in Fuseki database
+    */
+    public void setURI(final String datasetURI) {
+        this.uri = datasetURI;
     }
 }
