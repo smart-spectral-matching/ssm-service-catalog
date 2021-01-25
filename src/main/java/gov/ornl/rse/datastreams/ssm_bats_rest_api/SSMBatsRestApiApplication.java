e17833881b9db12668f2d676d0a4e38252ca3821
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-01-25 16:02:05 -0500

    Javadocs for Spring App entrypoint

--- src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/SSMBatsRestApiApplication.java
+++ src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/SSMBatsRestApiApplication.java
@@ -3,11 +3,17 @@ package gov.ornl.rse.datastreams.ssm_bats_rest_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class for Spring App - BATS REST API.
+*/
 @SpringBootApplication
 public class SSMBatsRestApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SSMBatsRestApiApplication.class, args);
-	}
-
+    /**
+     * Main method.
+     *
+     * @param args Arguments for SprintApplication run
+    */
+    public static void main(final String[] args) {
+        SpringApplication.run(SSMBatsRestApiApplication.class, args);
+    }
 }
