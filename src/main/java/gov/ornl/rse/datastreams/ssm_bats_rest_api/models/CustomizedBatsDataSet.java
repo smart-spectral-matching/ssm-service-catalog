4295d16910242aa5b6135341cbdb6c4a9e195003
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-07-09 12:35:38 -0400

    Adds the CustomizedBatsDataSet to extend setName method

---
+++ src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/models/CustomizedBatsDataSet.java
@@ -0,0 +1,17 @@
+package gov.ornl.rse.datastreams.ssm_bats_rest_api.models;
+
+import java.util.Locale;
+
+import gov.ornl.rse.bats.DataSet;
+
+public class CustomizedBatsDataSet extends DataSet {
+
+    /**
+     * Overrides the setName method of DataSet to always be lower-case.
+     *
+     * @param name
+    */
+    public void setName(final String name) {
+        super.setName(name.toLowerCase(new Locale("en")));
+    }
+}
