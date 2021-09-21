c7e66b9b466505ee2eafb9ecbf16fae10543edc6
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-09-21 14:33:49 -0400

    Adds docstring to ModelDocumentRepository

--- src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/repositories/ModelDocumentRepository.java
+++ src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/repositories/ModelDocumentRepository.java
@@ -5,6 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import gov.ornl.rse.datastreams.ssm_bats_rest_api.models.ModelDocument;
 
-public interface ModelDocumentRepository extends MongoRepository<ModelDocument, String>{
-    ModelDocument findBy_id(ObjectId _id);
+public interface ModelDocumentRepository extends MongoRepository<ModelDocument, String> {
+    /**
+     * Find ModelDocument from repository by ID.
+     *
+     * @param id ID of the ModelDocument
+     * @return ModelDocument object for the given ID
+     */
+    ModelDocument findById(ObjectId id);
 }
