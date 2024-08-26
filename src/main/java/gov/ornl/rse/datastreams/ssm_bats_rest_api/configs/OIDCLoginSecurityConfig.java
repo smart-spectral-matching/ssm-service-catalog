440efb2d58daa92ea2244fde900dd96aef36dec9
McDonnell, Marshall <mcdonnellmt@ornl.gov>
2024-08-26 15:37:57 -0400

    Set allow all for CORS origins, headers, and methods

--- src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/configs/OIDCLoginSecurityConfig.java
+++ src/main/java/gov/ornl/rse/datastreams/ssm_bats_rest_api/configs/OIDCLoginSecurityConfig.java
@@ -83,8 +83,9 @@ public class OIDCLoginSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*", "http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
